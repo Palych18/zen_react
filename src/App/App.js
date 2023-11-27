986ed5c0d41f3239253bf3c4856863bacd263e10
Palych18 <pavel_egoroff@mail.ru>
2023-11-27 21:55:34 +0300

    Added Download section
    Edited App file
    
    Added dependencies

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import './App.scss';
+import { Download } from '../widgets';
+import { useState, useEffect } from 'react';
+
+/**
+ * @function App
+ * @returns {JSX.Element}
+ */
 
 export const App = () => {
+  const [ downloadDetails, setDownloadDetails ] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('https://zen-npw-default-rtdb.firebaseio.com/.json');
+        const data = await response.json();
+        if (!data) throw new Error('No data!');
+        const { download } = data.en;
+        setDownloadDetails(download);
+      } catch (error) {
+        const /** @type {*} */ { message } = error;
+        console.error(message);
+      };
+    })();
+  }, []);
+
   return (
     <div className="App">
-      Zen React
+      {downloadDetails && (
+        <Download details={downloadDetails} />
+      )}
     </div>
   );
 };
---
+++ src/widgets/Download/Download.js
@@ -0,0 +1,54 @@
+import './Download.scss';
+import { getClassNames } from '../../shared/utils';
+
+/**
+ * @typedef {import('./types').DownloadProps} DownloadProps
+ */
+
+/**
+ * @function Download
+ * @param {DownloadProps} props
+ * @returns {JSX.Element}
+ */
+
+export const Download = (props) => {
+
+  const className = getClassNames(props.details.name);
+
+  return (
+    <section className={className}>
+      <div className={`${className}__wrapper`}>
+        <div className={`${className}__text-block`}>
+          <h1 className={`${className}__title`}>
+            {props.details.title.content}
+          </h1>
+          {props.details.texts.map((text) => (
+            <p className={`${className}__copy`}>{text}</p>
+          ))}
+          <div className={`${className}__links`}>
+            <a className={`${className}__link`}
+              href={props.details.links.apple.url}
+            >
+              <img src={props.details.links.apple.icon}
+                alt="Apple"
+              />
+            </a>
+            <a className={`${className}__link`}
+              href={props.details.links.google.url}
+            >
+              <img src={props.details.links.google.icon}
+                alt="Google"
+              />
+            </a>
+          </div>
+          <div className={`${className}__image-block`}>
+            <img className={`${className}__image`}
+              src={props.details.image.source}
+              alt={props.details.image.description}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
